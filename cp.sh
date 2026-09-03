35b684013be99c3f4c9256b794254268e54afe8a
L4afer <leaferfr@gmail.com>
2026-09-03 11:11:02 +0200

    first commit

---
+++ cp.sh
@@ -0,0 +1,10 @@
+cp -r ~/Downloads/i3dots-autumn/.config/betterlockscreen ~/.config/
+cp -r ~/Downloads/i3dots-autumn/.config/picom ~/.config/
+cp -r ~/Downloads/i3dots-autumn/.config/nvim ~/.config/
+cp -r ~/Downloads/i3dots-autumn/.config/polybar/ ~/.config/
+cp -r ~/Downloads/i3dots-autumn/.config/rofi ~/.config/
+cp -r ~/Downloads/i3dots-autumn/.config/neofetch ~/.config
+cp -r ~/Downloads/i3dots-autumn/.config/kitty ~/.config/
+cp -r ~/Downloads/i3dots-autumn/.config/kitty/.zshrc ~/
+cp -r ~/Downloads/i3dots-autumn/.config/i3 ~/.config/
+betterlockscreen -u ~/.config/i3/bench768p.png
