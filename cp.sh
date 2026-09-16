cf825a7aa4535aa0fa5017e0350aa5dc00c6b37f
L4afer <leaferfr@gmail.com>
2026-09-16 17:24:31 +0200

    Border color upgrade + wallpaper mess fixed

--- cp.sh
+++ cp.sh
@@ -6,6 +6,7 @@ cp -r ~/Downloads/i3dots-autumn/.config/rofi ~/.config/
 cp -r ~/Downloads/i3dots-autumn/.config/neofetch ~/.config
 cp -r ~/Downloads/i3dots-autumn/.config/kitty ~/.config/
 cp -r ~/Downloads/i3dots-autumn/.config/kitty/.zshrc ~/
+rm -rf ~/.config/i3
 cp -r ~/Downloads/i3dots-autumn/.config/i3 ~/.config/
 i3 reload
 ~/.config/i3/polr.sh
