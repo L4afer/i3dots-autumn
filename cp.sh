b2aefef012a1a8879ad701f175a1ef8a8a0b8795
L4afer <leaferfr@gmail.com>
2026-09-16 17:03:15 +0200

    cp.sh update

--- cp.sh
+++ cp.sh
@@ -7,4 +7,6 @@ cp -r ~/Downloads/i3dots-autumn/.config/neofetch ~/.config
 cp -r ~/Downloads/i3dots-autumn/.config/kitty ~/.config/
 cp -r ~/Downloads/i3dots-autumn/.config/kitty/.zshrc ~/
 cp -r ~/Downloads/i3dots-autumn/.config/i3 ~/.config/
+i3 reload
+~/.config/i3/polr.sh
 betterlockscreen -u ~/.config/i3/bench768p.png
